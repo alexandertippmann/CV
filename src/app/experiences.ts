cbaf91ceda0e70b8cb02312aa34568dff3f52b51
alexandertippmann <alexandertippmann@gmail.com>
2022-06-23 23:10:49 +0200

    fixing typos

--- src/app/experiences.ts
+++ src/app/experiences.ts
@@ -6,7 +6,7 @@ interface Experiences{
 }
 
 export const EDUCATION : Experiences[] = [
-  {location: "Justus-Liebig-Universität Gießen", dates: "2014-2017", position: "Bachelor of Science: Physics", description:"Bachelor's with focus on theoretical partical physics. (GPA: 3.0)"},
+  {location: "Justus-Liebig-Universität Gießen", dates: "2014-2017", position: "Bachelor of Science: Physics", description:"Bachelor's with focus on theoretical particle physics. (GPA: 3.0)"},
   {location: "Justus-Liebig-Universität Gießen", dates: "2017-2019", position: "Master of Science: Physics", description:"Master's degree with focus on Quantum Field Theory. (GPA 3.8)"},
   {location: "University of Washington", dates: "2017-2018", position: "Graduate Exchange in Physics", description:"One year exchange program with the University of Washington in Seattle."}
 ]
